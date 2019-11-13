17e567bf893d1603ba71ebc06f23d61c6c39f0ba
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-13 23:27:43 +0800

    now can get previewFile and upload files

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
-const upload = multer({ dest: "uploads/" });
-const p = require("path");
+const upload = multer({ dest: "yyy/" });
 
 const app = express();
 
-app.options("/upload", cors());
-app.put("/upload", cors(), upload.single("file"), function(req, res, next) {
-  res.json({ key: req.file.filename });
+app.post("/upload", cors(), upload.single("file"), function(req, res, next) {
+  console.log(req.file);
+  res.send("here");
 });
 app.get("/", cors(), function(req, res, next) {
   res.json({ key: "hello" });
 });
-app.get("/upload/:key", cors(), function(req, res, next) {
+app.get("/preview/:key", cors(), function(req, res, next) {
+  console.log(req.params.key);
   res.sendFile(
-    `uploads/${req.params.key}`,
+    `yyy/${req.params.key}`,
     {
       root: __dirname,
       headers: {
