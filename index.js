6141dd6befe152b21f6e0e80569457d4596be426
LarryKameZhao <larryzhao1994@outlook.com>
2019-11-13 22:50:34 +0800

    add test case

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-const express = require('express')
-const multer = require('multer')
-const cors = require('cors')
-const upload = multer({ dest: 'uploads/' })
-const p = require('path')
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const upload = multer({ dest: "uploads/" });
+const p = require("path");
 
-const app = express()
+const app = express();
 
-app.options('/upload', cors())
-app.put('/upload', cors(), upload.single('file'), function(req, res, next) {
-  res.json({ key: req.file.filename })
-})
-app.get('/upload/:key', cors(), function(req, res, next) {
+app.options("/upload", cors());
+app.put("/upload", cors(), upload.single("file"), function(req, res, next) {
+  res.json({ key: req.file.filename });
+});
+app.get("/", cors(), function(req, res, next) {
+  res.json({ key: "hello" });
+});
+app.get("/upload/:key", cors(), function(req, res, next) {
   res.sendFile(
     `uploads/${req.params.key}`,
     {
       root: __dirname,
       headers: {
-        'Content-Type': 'image/jpeg'
+        "Content-Type": "image/jpeg"
       }
     },
     error => {
       if (error) {
-        res.status(404).send('Not found')
+        res.status(404).send("Not found");
       }
     }
-  )
-})
-var port = process.env.PORT || 3000
+  );
+});
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log(port)
-  console.log('Example app listening on port 3000!')
-})
+  console.log(port);
+  console.log("Example app listening on port 3000!");
+});
